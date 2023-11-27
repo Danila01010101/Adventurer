ba5b94fd34f2246ea10f64b9ae040bed4e37655d
Danila Bokarev <danila228337228@gmail.com>
2023-11-27 12:31:30 +0300

    Basic wizard movement

--- Assets/Scripts/TopDownController.cs
+++ Assets/Scripts/TopDownController.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class TopDownController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CinemachineVirtualCamera mainCamera;
+    [SerializeField] private CinemachineBrain virtualCamera;
+    [SerializeField] private float speed;
+
+    private CharacterController characterController;
+    private Vector2 moveDirection;
+
+    private void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        mainCamera.transform.SetParent(null);
+        virtualCamera.transform.SetParent(null);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(-moveDirection.y, -9.1f, moveDirection.x);
+
+        characterController.Move(direction * Time.deltaTime * speed);
+    }
+
+    private void Update()
+    {
+        Move();
     }
 }

f235be9a507e7ce4ab4eda4ac67a081fc88ca35a
Danila <danila228337228@gmail.com>
2023-12-03 18:06:44 +0300

    Animation player added

---
+++ Assets/Scripts/Mage/MageAnimationNames.cs
@@ -0,0 +1,8 @@
+public static class MageAnimationNames
+{
+    public static string IdleState1 = "Idle01";
+    public static string IdleState2 = "Idle02";
+    public static string IdleState3 = "Idle03";
+    public static string WalkState = "WalkForward";
+    public static string BattleRunState = "BattleRunForward";
+}
---
+++ Assets/Scripts/Mage/MageAnimationPlayer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MageAnimationPlayer : MonoBehaviour
+{
+    private Animator animator;
+    private string currentState;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
+    }
+}

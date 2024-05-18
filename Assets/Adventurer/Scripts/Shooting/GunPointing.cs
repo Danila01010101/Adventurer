3cc6c85375413950e823407312d1e489eb8ea0dd
Danila <danila228337228@gmail.com>
2024-05-19 01:09:58 +0300

    Pointing mprovements  and debug

--- Assets/Adventurer/Scripts/Shooting/GunPointing.cs
+++ Assets/Adventurer/Scripts/Shooting/GunPointing.cs
@@ -1,16 +1,40 @@
 using Adventurer.Shooting;
-using ModestTree.Util;
 using UnityEngine;
 
 public class GunPointing : MonoBehaviour
 {
+    [SerializeField] private Camera playerCamera;
     [SerializeField] private ProceduralRecoil recoil;
+    [SerializeField] private Transform bulletSpawnPosition;
+    [SerializeField] private Vector3 defaultRotation;
+    [SerializeField] private Vector3 gunUpwardsDirection = Vector3.up;
+    [SerializeField] private Transform testAimTransform;
 
-    public Vector3 currentDirection = Vector3.zero;
-    public Vector3 GetCurrentDirection() => currentDirection;
+    private Vector3 aimGizmosPosition;
 
     private void Start()
     {
         recoil.Initialize(GetCurrentDirection);
+    }
+
+    public Vector3 GetCurrentDirection()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawRay(bulletSpawnPosition.transform.position, bulletSpawnPosition.transform.forward * 200, Color.red);
+            aimGizmosPosition = hit.point;
+            return defaultRotation;
+            return Quaternion.LookRotation(hit.point, gunUpwardsDirection).eulerAngles;
+        }
+
+        return defaultRotation;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(aimGizmosPosition, 0.2f);
     }
 }

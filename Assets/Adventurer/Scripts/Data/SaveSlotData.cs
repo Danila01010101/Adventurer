a8b42a5c5478c11044f757c3596c944d7c9d6d7a
Danila <64286216+Danila01010101@users.noreply.github.com>
2024-07-03 19:07:34 +0300

    SaveSlotData is now struct instead of class

--- Assets/Adventurer/Scripts/Data/SaveSlotData.cs
+++ Assets/Adventurer/Scripts/Data/SaveSlotData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Adventurer
 {
-    public class SaveSlotData
+    public struct SaveSlotData
     {
         public SceneID LastSceneIndex { get; private set; }
         public Vector3 LastPosition { get; private set; }

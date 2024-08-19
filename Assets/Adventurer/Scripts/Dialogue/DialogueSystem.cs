3694b0ee6b80c62a614901f1c3228d6bbdbd12f3
mivoky <66131852+mivoky@users.noreply.github.com>
2024-08-19 15:30:49 +0300

    BaseDialogueSystem

---
+++ Assets/Adventurer/Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public List<string> Replic;
+    public TextMeshProUGUI TextMeshProUGUI;
+
+    public int _numReplic;
+    void Start()
+    {
+        _numReplic = 0;
+        TextMeshProUGUI.text = Replic[_numReplic];
+    }
+
+    void Update()
+    {
+        Debug.Log(Replic.Count < _numReplic);
+        if (Input.GetMouseButtonDown(0) && Replic.Count - 1 > _numReplic)
+        {
+            _numReplic++;
+            TextMeshProUGUI.text = Replic[_numReplic];
+        }
+    }
+}
